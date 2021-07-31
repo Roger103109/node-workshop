1e99caf08e72ba21983094c607b84183ec3ece09
Roger103109 <roger103109@gmail.com>
2021-07-31 12:27:50 +0800

    sum.js

--- basic/sum.js
+++ basic/sum.js
@@ -1,5 +1,10 @@
 console.log("test");
 
+let totalresult=0;
 function sum(n){
-
+    for(i=0;i<=n;i++){
+        totalresult=i+totalresult;
+    }
+    return totalresult;
 };
+console.log(sum(10));
