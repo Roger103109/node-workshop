c9a289aa0911ecf99b2b5c4c78158bad07f3f737
Roger103109 <roger103109@gmail.com>
2021-08-15 10:15:36 +0800

    上傳

--- basic/sum.js
+++ basic/sum.js
@@ -1,12 +1,19 @@
 console.log("test");
 
+function sum(n) {
+  let totalresult = 0;
+  for (let i = 0; i <= n; i++) {
+    totalresult = i + totalresult;
+  }
+  return totalresult;
+}
 
-function sum(n){
-    let totalresult=0;
-    for(let i=0;i<=n;i++){
-        totalresult=i+totalresult;
-    }
-    return totalresult;
-};
+// function sum(n) {
+//   let totalresult = 0;
+//   for (let i = 0; i <= n; i++) {
+//     totalresult = i + totalresult;
+//   }
+//   return totalresult;
+// }
 console.log(sum(1));
 console.log(sum(10));
